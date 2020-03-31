b4761ef7c7bac49762fbb001df28ff49c7c3f43f
arodriguuij <arodriguuij@gmail.com>
2020-03-31 09:37:08 +0100

    updating our favicon and title

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -12,10 +12,10 @@ export const selectHidden = createSelector(
   cart => cart.hidden
 )
 
-export const selectCartItemsCount = createSelector([selectCart], cart =>
-  cart.cartItems.reduce((acc, cartItem) => acc + cartItem.quantity, 0)
+export const selectCartItemsCount = createSelector([selectCartItems], cartItems =>
+  cartItems.reduce((acc, cartItem) => acc + cartItem.quantity, 0)
 );
 
-export const selectCartTotal = createSelector([selectCart], cart =>
-  cart.cartItems.reduce((acc, cartItem) => acc + cartItem.quantity * cartItem.price, 0)
+export const selectCartTotal = createSelector([selectCartItems], cartItems =>
+  cartItems.reduce((acc, cartItem) => acc + cartItem.quantity * cartItem.price, 0)
 );
