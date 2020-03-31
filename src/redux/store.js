e537623ec4f19e8c0e15516293d5cb5116502d37
arodriguuij <arodriguuij@gmail.com>
2020-03-31 11:10:52 +0100

    added cart store to localstore

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
-
+import { persistStore } from "redux-persist";
 import rootReducer from "./root-reducer";
 
 const middlewares = [];
@@ -8,5 +8,6 @@ const middlewares = [];
 if (process.env.NODE_ENV === "development") middlewares.push(logger);
 
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
+const persistor = persistStore(store);
 
-export default store;
+export { store, persistor };
