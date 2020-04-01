7d23c2a02da00ea8fd55c55eabe675341af59a66
arodriguuij <arodriguuij@gmail.com>
2020-04-01 23:43:07 +0100

    updating our code to be pwa compliant

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { Provider } from "react-redux";
 import "./index.css";
 import { PersistGate } from "redux-persist/integration/react";
 import { store, persistor } from "./redux/store";
+import * as serverWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -19,3 +20,5 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
+
+serverWorker.register();
