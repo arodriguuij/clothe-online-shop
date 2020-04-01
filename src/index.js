7c235be72b8e5b0a4a429ca82536a188666c6468
arodriguuij <arodriguuij@gmail.com>
2020-04-02 00:18:38 +0100

    added react-https-redirect

--- src/index.js
+++ src/index.js
@@ -6,18 +6,21 @@ import { Provider } from "react-redux";
 import "./index.css";
 import { PersistGate } from "redux-persist/integration/react";
 import { store, persistor } from "./redux/store";
-import * as serverWorker from './serviceWorker';
+import HttpsRedirect from "react-https-redirect";
+import * as serverWorker from "./serviceWorker";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <PersistGate persistor={persistor}>
-          <App />
-        </PersistGate>
-      </BrowserRouter>
-    </Provider>
-  </React.StrictMode>,
+  <HttpsRedirect>
+    <React.StrictMode>
+      <Provider store={store}>
+        <BrowserRouter>
+          <PersistGate persistor={persistor}>
+            <App />
+          </PersistGate>
+        </BrowserRouter>
+      </Provider>
+    </React.StrictMode>
+  </HttpsRedirect>,
   document.getElementById("root")
 );
 
